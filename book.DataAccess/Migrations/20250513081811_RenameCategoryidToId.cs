fac76af768ab26331007d4fa01244c628c802a0c
7Waqas <waqashamid3332002@gmail.com>
2025-05-17 02:31:51 -0700

    Initial commit with form changes

---
+++ book.DataAccess/Migrations/20250513081811_RenameCategoryidToId.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace book.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameCategoryidToId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Rename column 'Categoryid' to 'Id' in 'Categories' table
+            migrationBuilder.RenameColumn(
+                name: "Categoryid",   // Old column name
+                table: "Categories",  // Table name
+                newName: "Id");       // New column name
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Revert the column name from 'Id' back to 'Categoryid' in case of rollback
+            migrationBuilder.RenameColumn(
+                name: "Id",           // New column name
+                table: "Categories",  // Table name
+                newName: "Categoryid"); // Old column name
+        }
+    }
+}
---
+++ book.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using book.DataAccess.Data;
+using book.DataAccess.Repository.IRepository;
+using book.Models;
+
+namespace book.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            var objFromDb = _db.Products.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.ISBN = obj.ISBN;
+                objFromDb.Title = obj.Title;
+                objFromDb.Author = obj.Author;
+                objFromDb.Description = obj.Description;
+                objFromDb.ListPrice = obj.ListPrice;
+                objFromDb.Price = obj.Price;
+                objFromDb.Price50 = obj.Price50;
+                objFromDb.Price100 = obj.Price100;
+                if (obj.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+
+            }
+        }
+    }
+}
